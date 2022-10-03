f261be1ea000699f8ac410488d9bc0d2d78362fb
hthelander <112781292+hthelander@users.noreply.github.com>
2022-10-03 17:31:17 -0400

    Saving Changes

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,15 +10,30 @@ with open(csvpath) as csvfile:
     header = next(election_data)
 
     candidate_list = []
+    # candidate_votes = 
 
     total_votes = 0
 
+      
 
     for row in election_data:
         
         total_votes = total_votes + 1
         candidate = str(row[2])
+
+
+        
+        for i in candidate_list:
+            if candidate != i:
+                candidate_list.append(candidate)
         candidate_list.append(candidate)
+                
+
+    print(total_votes)
+    print(candidate_list[2])
+                  
+        
+
         
         
 
